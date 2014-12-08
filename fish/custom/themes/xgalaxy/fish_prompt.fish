ab1ea61fb8d776bb8c6d6f4ea83c30a35691ae3c
Robert D. Blanchet Jr <xgalaxy@gmail.com>
2014-12-08 09:33:59 -0800

    Add newline before prompt.

--- fish/custom/themes/xgalaxy/fish_prompt.fish
+++ fish/custom/themes/xgalaxy/fish_prompt.fish
@@ -20,6 +20,7 @@ set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
 set -g __fish_git_prompt_color_cleanstate green bold
 
 function fish_prompt
+	echo
 	set_color yellow
 	printf '%s' (whoami)
 	set_color normal
